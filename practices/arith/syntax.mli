7b9d5124ece96ad3fc996ffe32096674ce94c981
maplgebra <maple_@outlook.com>
2021-04-02 20:49:42 +0800

    update

---
+++ practices/arith/syntax.mli
@@ -0,0 +1,27 @@
+(* module Syntax: syntax trees and associated support functions *)
+
+open Support.Pervasive
+open Support.Error
+
+(* Data type definitions *)
+type term =
+    TmTrue of info
+  | TmFalse of info
+  | TmIf of info * term * term * term
+  | TmZero of info
+  | TmSucc of info * term
+  | TmPred of info * term
+  | TmIsZero of info * term
+
+type command =
+  | Eval of info * term
+
+
+
+(* Printing *)
+val printtm: term -> unit
+val printtm_ATerm: bool -> term -> unit
+
+(* Misc *)
+val tmInfo: term -> info
+
